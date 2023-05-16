130a8bcd415cc01466a582c8ef28b099a7b10a3f
OzimanD <89782267+OzimanD@users.noreply.github.com>
2023-05-16 19:36:40 +0300

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -10,13 +10,15 @@ public class Test {
         System.out.println("please input variable");
     }
 
-    static int FindSpeed(int t, int v){//параметри
+    static int FindSpeed(int t, int v){//ГЇГ Г°Г Г¬ГҐГІГ°ГЁ
         int s = v*t;
         return s;
     }
 
+    
+    // i need coments for this method
     public static void main(String[] args) {
-        int res = FindSpeed(2,70);// аргументи
+        int res = FindSpeed(2,70);// Г Г°ГЈГіГ¬ГҐГ­ГІГЁ
         int prise = res * 7;
         System.out.println(prise);
         System.out.println(FindSpeed(7,9));
